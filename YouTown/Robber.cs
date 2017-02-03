d57a9bcc672107d7c12744f9a0e7934ce51b79f6
poutruu <ruud.poutsma@bakerhughes.com>
2017-02-03 22:05:30 +0100

    implement robber

---
+++ YouTown/Robber.cs
@@ -0,0 +1,21 @@
+﻿namespace YouTown
+{
+    /// <summary>
+    /// Token placeable on any <see cref="IHex"/> where the <see cref="IHex.CanHaveRobber"/>
+    /// is true. 
+    /// </summary>
+    /// If there are any <see cref="Desert"/> at the board at start, the robber should be randomly placed
+    /// on any of available <see cref="Desert"/>s.
+    public class Robber : IGameItem
+    {
+        public Robber(int id)
+        {
+            Id = id;
+        }
+
+        /// <inheritdoc />
+        public int Id { get; }
+
+        public Location Location { get; set; }
+    }
+}
