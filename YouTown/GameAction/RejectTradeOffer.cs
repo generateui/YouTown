cb842d697c1f1753a2d9503165eae197cbbdae6b
poutruu <ruud.poutsma@bakerhughes.com>
2017-02-07 20:23:18 +0100

    implement RejectTradeOffer

---
+++ YouTown/GameAction/RejectTradeOffer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using YouTown.Validation;
+
+namespace YouTown.GameAction
+{
+    public class RejectTradeOffer : IGameAction
+    {
+        public RejectTradeOffer(int id, IPlayer player, TradeOffer tradeOffer)
+        {
+            Id = id;
+            Player = player;
+            TradeOffer = tradeOffer;
+        }
+
+        public int Id { get; }
+        public IPlayer Player { get; }
+        public ITurnPhase TurnPhase { get; private set; }
+        public IGamePhase GamePhase { get; private set; }
+        public ITurn Turn { get; private set; }
+        public TradeOffer TradeOffer { get; }
+        public bool IsAllowedInOpponentTurn => true;
+
+        public bool IsAllowedInTurnPhase(ITurnPhase turnPhase) => turnPhase.IsTrading;
+        public bool IsAllowedInGamePhase(IGamePhase gamePhase) => gamePhase.IsTurns;
+
+        public IValidationResult Validate(IGame game) =>
+            new ValidateAll()
+                .WithObject<NotNull>(TradeOffer)
+                .With<NotRespondedYet, IPlayer, TradeOffer>(Player, TradeOffer)
+                .Validate();
+
+        public void PerformAtServer(IServerGame serverGame)
+        {
+        }
+
+        public void Perform(IGame game)
+        {
+            var rejectOffer = new Reject(Player);
+            TradeOffer.Responses.Add(rejectOffer);
+
+            TurnPhase = game.PlayTurns.TurnPhase;
+            GamePhase = game.GamePhase;
+            Turn = game.PlayTurns.Turn;
+        }
+    }
+}
---
+++ YouTown/Validation/NotRespondedYet.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+
+namespace YouTown.Validation
+{
+    public class NotRespondedYet : ValidatorBase<IPlayer, TradeOffer>
+    {
+        public override IValidationResult Validate(IPlayer player, TradeOffer tradeOffer, string text = null)
+        {
+            var responsesOfPlayer = tradeOffer.Responses.Where(tr => tr.Player.Equals(player));
+            if (responsesOfPlayer.Any())
+            {
+                return new Invalid($"player {player.User.Name} already responded");
+            }
+            return Validator.Valid;
+        }
+    }
+}
