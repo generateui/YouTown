884f99bb96c136b25dca3931aaba239d3c9ff628
poutruu <ruud.poutsma@bakerhughes.com>
2017-02-08 17:08:42 +0100

    implement Game

--- YouTown/IGame.cs
+++ YouTown/IGame.cs
@@ -26,4 +26,57 @@ namespace YouTown
     {
         int VictoryPointsToWin { get; set; }
     }
+
+    public class GameOptions : IGameOptions
+    {
+        public int VictoryPointsToWin { get; set; }
+    }
+
+    public class Game : IGame
+    {
+        private readonly List<IGamePhase> _gamePhases;
+
+        public Game(IBoard board, IBank bank, IPlayerList players)
+        {
+            Board = board;
+            Bank = bank;
+            Players = players;
+            GamePhase = DetermineFirstPlayer;
+            _gamePhases = new List<IGamePhase>
+            {
+                DetermineFirstPlayer,
+                SetupGamePhase,
+                PlaceInitialPieces,
+                PlayTurns,
+                EndOfGame
+            };
+        }
+
+        public IBoard Board { get; }
+        public IBank Bank { get; }
+        public IPlayerList Players { get; }
+        public IGameOptions Options { get; } = new GameOptions();
+        public IList<Chat> Chats { get; } = new List<Chat>();
+        public IActionQueue Queue { get; } = new ActionQueue();
+        public IIdentifier Identifier { get; } = new Identifier();
+        public IGamePhase GamePhase { get; private set; }
+        public DetermineFirstPlayer DetermineFirstPlayer { get; } = new DetermineFirstPlayer();
+        public SetupGamePhase SetupGamePhase { get; } = new SetupGamePhase();
+        public PlaceInitialPieces PlaceInitialPieces { get; } = new PlaceInitialPieces();
+        public PlayTurns PlayTurns { get; } = new PlayTurns();
+        public EndOfGame EndOfGame { get; } = new EndOfGame();
+
+        public void MoveToNextPhase()
+        {
+            var index = _gamePhases.IndexOf(GamePhase);
+            if (index - 1 >= _gamePhases.Count)
+            {
+                return;
+            }
+            var newIndex = index + 1;
+            GamePhase.End(this);
+            GamePhase = _gamePhases[newIndex];
+            GamePhase.Start(this);
+        }
+    }
 }
